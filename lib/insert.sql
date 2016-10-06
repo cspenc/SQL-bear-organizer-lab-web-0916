dea375e39a3b1267f87edc5db245e94a01d9b1dc
cspenc <crs992@gmail.com>
2016-10-06 14:21:26 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "brown", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 15, "F", "yellow", "sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "F", "orange", "constipated", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 25, "M", "brown", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "M", "blue", "sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 7, "M", "brown", "happyish", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 89, "F", "purple", "having a good time", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 101, "F", "rainbow", "fine", 1);
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed
